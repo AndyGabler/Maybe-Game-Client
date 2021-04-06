743062a148ac7de51a2d87af4f0aeb2b82368968
Andy Gabler <andrewpgabler@gmail.com>
2021-04-06 13:56:09 -0400

    Current key exchange implementation. Uses Diffie-Helman.

---
+++ src/com/gabler/gameclient/app/AppStart.java
@@ -0,0 +1,8 @@
+package com.gabler.gameclient.app;
+
+public class AppStart {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ test/java/com/gabler/gameclient/dhke/DhkeClientDriverTest.java
@@ -0,0 +1,29 @@
+package com.gabler.gameclient.dhke;
+
+import com.gabler.client.ClientStartException;
+
+import java.math.BigInteger;
+
+/**
+ * Bootstrap a DHKE client.
+ *
+ * @author Andy Gabler
+ */
+public class DhkeClientDriverTest {
+
+    public static void main(String[] args) throws ClientStartException {
+        final DhkeClient client = new DhkeClient(DhkeClientDriverTest::printKeyBytesAndId);
+        client.start("localhost");
+        client.requestNewKey();
+    }
+
+    private static void printKeyBytesAndId(byte[] bytes, String keyId) {
+        System.out.println("DHKE Byte array: ");
+        for (byte b : bytes) {
+            System.out.print(String.format("0x%02X ", b));
+        }
+
+        System.out.println("\nDHKE Integer Form " + new BigInteger(bytes).toString());
+        System.out.println("Key ID: " + keyId);
+    }
+}
