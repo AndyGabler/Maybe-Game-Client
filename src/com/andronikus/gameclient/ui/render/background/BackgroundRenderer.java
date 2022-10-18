1b0edecbccc20b25b481b1b60b0835aaeb7efe1d
Andy Gabler <andrewpgabler@gmail.com>
2022-10-17 20:27:34 -0400

    25. Remove chunking code, it just causes lag.

--- src/com/andronikus/gameclient/ui/render/background/BackgroundRenderer.java
+++ src/com/andronikus/gameclient/ui/render/background/BackgroundRenderer.java
@@ -12,9 +12,6 @@ import java.util.ArrayList;
 
 public class BackgroundRenderer {
 
-
-
-    private ArrayList<ArrayList<BufferedImage>> tiles = null;
     private final BufferedImage masterImage;
     private final int totalWidth;
     private final int totalHeight;
@@ -29,61 +26,7 @@ public class BackgroundRenderer {
         this.chunkHeight = chunkHeight;
     }
 
-    /**
-     * Pre-calculate the tiles on the image.
-     */
-    public void prechunkImage() {
-        chunkImage();
-        for (int tileX = 0; tileX < tiles.size(); tileX++) {
-            for (int tileY = 0; tileY < tiles.get(tileX).size(); tileY++) {
-                calculateOrGetChunk(tileX, tileY);
-            }
-        }
-    }
-
-    /**
-     * Generate the spots for the tiles, without necessarily calculating what goes in them.
-     */
-    private void chunkImage() {
-        int chunkXCount = totalWidth / chunkWidth;
-        if (totalWidth % chunkWidth != 0) {
-            chunkXCount++;
-        }
-        int chunkYCount = totalWidth / chunkHeight;
-        if (totalHeight % chunkHeight != 0) {
-            chunkYCount++;
-        }
-
-        tiles = new ArrayList<>(chunkXCount);
-        for (int chunkX = 0; chunkX < chunkXCount; chunkX++) {
-            final ArrayList<BufferedImage> tileColumn = new ArrayList<>(chunkYCount);
-            for (int chunkY = 0; chunkY < chunkYCount; chunkY++) {
-                tileColumn.add(null);
-            }
-            tiles.add(tileColumn);
-        }
-    }
-
-    /**
-     * Calculate (or get the cached result) of what goes in a chunk.
-     *
-     * @param chunkX The Chunk X
-     * @param chunkY The Chunk Y
-     * @return The image in the chunk, whether newly split off or cached
-     */
-    private BufferedImage calculateOrGetChunk(int chunkX, int chunkY) {
-        BufferedImage result = tiles.get(chunkX).get(chunkY);
-        if (result == null) {
-            result = masterImage.getSubimage(chunkX * chunkWidth, chunkY * chunkHeight, chunkWidth, chunkHeight);
-            tiles.get(chunkX).set(chunkY, result);
-        }
-        return result;
-    }
-
-    public void render(Graphics graphics, Player mainPlayer, BoundingBoxBorder border, long width, long height, GameWindow observer) {
-        if (tiles == null) {
-            // chunkImage();
-        }
+    public void render(Graphics graphics, Player mainPlayer, BoundingBoxBorder border, GameWindow observer) {
 
         // To keep the edge the same for all players, we keep the same amount of pixels reserved, however, this determines the scaling of the background
         final double widthScale = observer.getAspectRatio().getWidthScale();
